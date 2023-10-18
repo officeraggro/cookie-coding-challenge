034ba128e6cb7e6b6f9a4df75ba7c6e241f1b90a
officeraggro <countringworm@gmail.com>
2023-10-18 16:39:49 -0500

    Added routes

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 var cookieParser = require("cookie-parser");
 
-const app = express()
-const port = 8888
+const app = express();
+const port = 8888;
 
-app.use(cors())
-app.use(cookieParser())
+app.use(cors());
+app.use(cookieParser());
 
-app.get('/hello', (req, res) => {
+app.get("/login", (req, res) => {
+  res.cookie("name", "foobar");
+  res.status(200).send("You got a cookie!");
+});
 
-    res.cookie("name", "foobar")
-    if (req.cookies.name !== undefined) {
-        res.status(200).send(`Hello, ${req.cookies.name}!`)
-    } else {
-        res.status(403).send('Come back with cookies!')
-    }
-})
+app.get("/hello", (req, res) => {
+  if (req.cookies.name !== undefined) {
+    res.status(200).send(`Welcome, ${req.cookies.name}!`);
+  } else {
+    res.status(403).send("Come back with cookies!");
+  }
+});
 
 app.listen(port, () => {
-    console.log('Express server started on port ', port)
-})
+  console.log("Express server started on port ", port);
+});
