7db2f636eb1a1314c9f68b386182935ea27fa633
officeraggro <countringworm@gmail.com>
2023-10-18 16:44:11 -0500

    Modified package.json

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-var cookieParser = require("cookie-parser");
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = 8888;
