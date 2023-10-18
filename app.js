f6d0bd258cefd234eadb6b06f2e90bb5fe865e67
officeraggro <countringworm@gmail.com>
2023-10-18 15:20:01 -0500

    Created Express server

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const cors = require('cors')
+var cookieParser = require("cookie-parser");
 
 const app = express()
 const port = 8888
 
+app.use(cors())
+app.use(cookieParser())
+
 app.get('/', (req, res) => {
-    res.send('Hello doofus!')
+
+    res.cookie("name", "foobar")
+    if (req.cookies.name !== undefined) {
+        res.status(200).send(`Hello, ${req.cookies.name}!`)
+    } else {
+        res.status(403).send('Come back with cookies!')
+    }
 })
 
 app.listen(port, () => {
